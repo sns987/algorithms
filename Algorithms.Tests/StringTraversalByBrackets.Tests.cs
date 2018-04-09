6f78d729188a87419d9f89775def5197405673f5
Nadezhda <sveklina@skbkontur.ru>
2018-04-09 20:11:04 +0500

    add Stacks: Balanced Brackets

---
+++ Algorithms.Tests/StringTraversalByBrackets.Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Algorithms.Tests
+{
+    [TestFixture]
+    public class RunStringTraversalByBrackets
+    {
+        private const string SuccessAnswer = "YES";
+        private const string UnSuccessAnswer = "NO";
+
+        [SetUp]
+        public void Init()
+        {
+        }
+        
+
+        [Test]
+        public void RunStringTraversalByBracketsTest()
+        {
+            var lines = new[]
+            {
+                "{[()]}",
+                "{[(])}",
+                "{{[[(())]]}}"
+            };
+
+            var expectedResults = new[]
+            {
+                SuccessAnswer,
+                UnSuccessAnswer,
+                SuccessAnswer
+            };
+
+            for(var i = 0; i < lines.Length; i++)
+            {
+                var result = StringTraversalByBrackets.Traverse(lines[i], SuccessAnswer, UnSuccessAnswer);
+                Assert.AreEqual(result, expectedResults[i]);
+            }
+        }
+    }
+}
---
+++ Algorithms/StringTraversalByBrackets.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+    public class StringTraversalByBrackets
+    {
+        private static readonly Dictionary<char, char> Brackets = new Dictionary<char, char>
+        {
+            {'{', '}'},
+            {'[', ']'},
+            {'(', ')'}
+        };
+
+        public static string Traverse(string expression, string successAnswer, string unSuccessAnswer)
+        {
+            var res = new Stack<char>();
+
+            foreach (var symbol in expression)
+            {
+                // пришла открывающая скобка
+                if (IsOpenBracket(symbol))
+                    res.Push(symbol);
+
+                if (!IsClosedBracket(symbol))
+                    continue;
+
+                if (IsStringBeginFromClosedSymbol(res))
+                    return unSuccessAnswer;
+
+                var top = res.Pop();
+                var closedTopPair = Brackets[top];
+                if (closedTopPair != symbol)
+                    return unSuccessAnswer;
+            }
+
+            return IsOpenBracketsMoreThanClosed(res)
+                ? unSuccessAnswer
+                : successAnswer;
+        }
+
+        private static bool IsClosedBracket(char symbol)
+        {
+            return Brackets.Values.Contains(symbol);
+        }
+
+        private static bool IsOpenBracket(char symbol)
+        {
+            return Brackets.Keys.Contains(symbol);
+        }
+
+        private static bool IsOpenBracketsMoreThanClosed(IEnumerable<char> res)
+        {
+            return res.Any();
+        }
+
+        private static bool IsStringBeginFromClosedSymbol(IEnumerable<char> res)
+        {
+            return !res.Any();
+        }
+    }
+}
